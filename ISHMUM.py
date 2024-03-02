5ac4cc45c21bbb2f8128fd74fe9cb9a815a21454
Ishmum15 <124346030+Ishmum15@users.noreply.github.com>
2024-03-02 23:05:47 +0600

    Create ISHMUM.py

---
+++ ISHMUM.py
@@ -0,0 +1,13 @@
+#Fuck your Hot Sister
+#Sex+Sexy= Sexual baby opps
+import os, sys, platform,time
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import ISHMUM    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import ISHMUM32
